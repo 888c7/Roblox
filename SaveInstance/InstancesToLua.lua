c534ff7bfc23fb747e2dc91e29f6b611d89f0c96
v1zoy <102887636+v1zoy@users.noreply.github.com>
2024-04-24 13:09:01 -0400

    Update InstancesToLua.lua

--- SaveInstance/InstancesToLua.lua
+++ SaveInstance/InstancesToLua.lua
@@ -10,18 +10,22 @@ loadstring(game:HttpGet("https://github.com/v1zoy/Roblox/edit/main/SaveInstance/
 - roblox studio
 loadstring(game.HttpService:GetAsync("https://github.com/v1zoy/Roblox/edit/main/SaveInstance/InstancesToLua.lua"))( Instance )
 
---]]
+]] --
 
-function request(url)
-	if game.RunService:IsServer() then
-		return game.HttpService:GetAsync(url);
-	else
+local RS = game:GetService("RunService")
+local HS = game:GetService("HttpService")
+
+function getRequest(url)
+	local success, error = pcall(function()
 		return game:HttpGet(url);
+	end)
+	if not success then
+		return HS:GetAsync(url);
 	end
-end)
+end
 function httpPostRequest(jsonData)
-	if game.RunService:IsServer() then
-		game.HttpService:PostAsync('http://172.16.0.31:80', jsonData)
+	if RS:IsServer() then
+		HS:PostAsync('http://172.16.0.31:80', jsonData)
 	else
 		syn.request({
 			Url = 'http://172.16.0.31:80',
@@ -32,12 +36,22 @@ function httpPostRequest(jsonData)
 			Body = jsonData
 		})
 	end	
-end)
+end
 
-local PropertyToString = loadstring(request("https://raw.githubusercontent.com/v1zoy/Roblox/main/SaveInstance/PropertyToString.lua"))()
-local API = loadstring(request("https://raw.githubusercontent.com/v1zoy/Roblox/main/SaveInstance/API.lua"))()
+local PropertyToString = loadstring(getRequest("https://raw.githubusercontent.com/v1zoy/Roblox/main/SaveInstance/PropertyToString.lua"))()
+local API = loadstring(getRequest("https://raw.githubusercontent.com/v1zoy/Roblox/main/SaveInstance/API.lua"))()
 
 local apiFetched = false
+local classConverter = {
+	'TouchTransmitter', 
+	'Workspace',
+	'Terrain',
+	'StarterGui',
+	'Players',
+	'ServerScriptService',
+	'ServerStorage',
+	'Lightning',
+}
 local function instanceToLua(part)
 	local selectedItem = part
 	local awaitReference = {}
@@ -64,34 +78,31 @@ local function instanceToLua(part)
 	local ref = {}
 	local idCount = 0
 	local objectIds = {}
-	local function GetProperties(obj)
-		local properties = {}
-		local default = defaultObjects[obj.ClassName]
-		local class = API.ClassesByName[obj.ClassName]
+	local function GetProperties(obj, newClassName)
+		local properties,default,class = {},defaultObjects[newClassName],API.ClassesByName[newClassName]
 		for propName,propInfo in pairs(class:GetAllProperties(true)) do
 			if ((not propInfo.ReadOnly) and (not propInfo.Hidden) and propName ~= "Parent") then
 				local val = obj[propName]
-				if (default[propName] ~= val) then
-					local valStr, isRef = PropertyToString(propInfo.ValueType, val, propName)
-					if (isRef) then
-						properties[propName] = ("\"_R:%s_\""):format(objectIds[val] or "E")
-					else
-						properties[propName] = valStr
-					end
-				end
+				if (default[propName] == val) then continue end
+				local valStr, isRef = PropertyToString(propInfo.ValueType, val, propName)
+				if (isRef) then properties[propName] = ("\"_R:%s_\""):format(objectIds[val] or "E") else properties[propName] = valStr end
 			end
 		end
 		return properties
 	end
 	local function Scan(obj, indentLvl)
-		local b = {'TouchTransmitter', 'Workspace'}
-		if table.find(b,obj.ClassName) then return end
+		local newClassName = obj.ClassName
+		if table.find(classConverter, obj.ClassName) then 
+			newClassName = "Folder"
+		end
+
 		local indent = ("\t"):rep(indentLvl)
 		if (indentLvl ~= 0) then
 			codeBuilder[#codeBuilder + 1] = "\n" .. indent
 		end
-		codeBuilder[#codeBuilder + 1] = "{\n" .. indent .. "\tID = " .. objectIds[obj] .. ";\n" .. indent .. "\tType = \"" .. obj.ClassName .. "\";\n" .. indent .. "\tProperties = {"
-		local props = GetProperties(obj)
+		print(objectIds[obj], newClassName)
+		codeBuilder[#codeBuilder + 1] = "{\n" .. indent .. "\tID = " .. objectIds[obj] .. ";\n" .. indent .. "\tType = \"" .. newClassName .. "\";\n" .. indent .. "\tProperties = {"
+		local props = GetProperties(obj, newClassName)
 		if (next(props)) then
 			for propName,propVal in pairs(props) do
 				codeBuilder[#codeBuilder + 1] = "\n" .. indent .. "\t\t" .. propName .. " = " .. propVal .. ";"
@@ -120,8 +131,8 @@ local function instanceToLua(part)
 	Scan(selectedItem, 0)
 	codeBuilder[#codeBuilder + 1] = "local function Scan(item, parent) local obj = Instance.new(item.Type) if (item.ID) then local awaiting = awaitRef[item.ID] if (awaiting) then awaiting[1][awaiting[2]] = obj awaitRef[item.ID] = nil else partsWithId[item.ID] = obj end end for p,v in pairs(item.Properties) do if (type(v) == \"string\") then local id = tonumber(v:match(\"^_R:(%w+)_$\")) if (id) then if (partsWithId[id]) then v = partsWithId[id] else awaitRef[id] = {obj, p} v = nil end end end obj[p] = v end for _,c in pairs(item.Children) do Scan(c, obj) end obj.Parent = parent return obj end\nScan(root, workspace)"
 	local source = table.concat(codeBuilder, "")
-	
-	local jsonData = game:GetService('HttpService'):JSONEncode({script = source})
+
+	local jsonData = HS:JSONEncode({script = source})
 	print("Saving Instance(s)...")
 	httpPostRequest(jsonData)
 end
