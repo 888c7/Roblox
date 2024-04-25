c74679fd87f60d08e237a8fcc4d1967ae061c328
v1zoy <102887636+v1zoy@users.noreply.github.com>
2024-04-24 22:34:22 -0400

    Update minify.lua

--- SaveInstance/minify.lua
+++ SaveInstance/minify.lua
@@ -1,21 +1,38 @@
-function getRequest(url)
-	local success, error = pcall(function()
-		return game:HttpGet(url);
-	end)
-	if not success then
-		return game:GetService("HttpService"):GetAsync(url);
+local function __get(_) return game:GetService('HttpService'):GetAsync(_) end
+local request = {get=(pcall(function()return game.HttpGet end) and game.HttpGet or __get)}
+
+local server = {
+	post = function(jsonData)
+		if not game:GetService("RunService"):IsServer() then
+			syn.request({
+				Url = 'http://172.16.0.31:80', Method = "POST",
+				Headers = {["Content-Type"] = "application/json"},
+				Body = jsonData
+			})
+		else game:GetService("HttpService"):PostAsync('http://172.16.0.31:80', jsonData) end
+	end,
+	json = function(source, root)
+		return game:GetService("HttpService"):JSONEncode({script = source, name = game.placeId .. '-root-game.' .. root.Properties.Name})
 	end
-end
+}
 function import(script_name)
-	local rawscript = getRequest(`https://raw.githubusercontent.com/v1zoy/Roblox/main/SaveInstance/{script_name}.lua`)
+	local rawscript = request.get(`https://raw.githubusercontent.com/v1zoy/Roblox/main/SaveInstance/{script_name}.lua`)
 	return loadstring(rawscript)
 end
 
 local table = ({...})[1]
 local minify = ({...})[2]
 
-return import('TableToString')({
+local _script = import('TableToString')({
 	table   = table,
 	newline = (minify == false and true or false),
 	tab     = (minify == false and true or false),
 })
+
+if minify then
+	local data = server.json(_script, table)
+	server.post(data)
+end
+
+print(_script)
+return _script
